e9c4900c758e9f92e17bf9cfb6adab2b3d713336
arcstep <43801@qq.com>
2024-03-27 21:51:38 +0800

    Update version to 4.0.21 and add retrieval and web search tools

--- langchain_zhipu/__init__.py
+++ langchain_zhipu/__init__.py
@@ -2,6 +2,7 @@
 from ._client import ZhipuAI
 from .chat import ChatZhipuAI
 from .embeddings import ZhipuAIEmbeddings
+from .utils import convert_to_retrieval_tool, convert_to_web_search_tool
 
 from .core._errors import (
     ZhipuAIError,
