9db6f64a2c2c8f22b40fea8bd14839d7394b6f6a
arcstep <43801@qq.com>
2024-04-20 19:18:01 +0800

    Refactor langchain_zhipu/__init__.py to update imports and remove unused code

--- langchain_zhipu/__init__.py
+++ langchain_zhipu/__init__.py
@@ -5,36 +5,15 @@
 from langchain_zhipu import (
     ChatZhipuAI,               # 用于V4版本接口的通用大模型对话
     ZhipuAIEmbeddings,         # 用户V4版本接口的向量模型
-    KnowledgeChatZhipuAI,      # 使用对话模式直接访问基于知识库的大模型应用
+    KnowledgeChatZhipuAI,      # 使用对话模式直接访问基于知识库的大模型应用（实际上访问了V3大模型接口）
     KnowledgeManagerZhipuAI,   # 用于新接口的知识库管理的REST接口
     convert_to_retrieval_tool, # 声明通用大模型中自带的知识库检索的工具
     convert_to_web_search_tool # 声明通用大模型中自带的Web搜索的工具
 )
 ```
-
-```
-如果要使用 4.0.x 版本的 ChatZhipuAI 和 ZhipuAIEmbeddings 可以按如下方式导出：
-from langchain_zhipu.chat import ChatZhipuAI
-from langchain_zhipu.embeddings import ZhipuAIEmbeddings
-```
 """
-from ._client import ZhipuAI
 from .http.manager import KnowledgeManagerZhipuAI
 from .http.chat import ChatZhipuAI, KnowledgeChatZhipuAI
 from .http.embeddings import ZhipuAIEmbeddings
 from .utils import convert_to_retrieval_tool, convert_to_web_search_tool
-
-from .core._errors import (
-    ZhipuAIError,
-    APIStatusError,
-    APIRequestFailedError,
-    APIAuthenticationError,
-    APIReachLimitError,
-    APIInternalError,
-    APIServerFlowExceedError,
-    APIResponseError,
-    APIResponseValidationError,
-    APITimeoutError,
-)
-
 from .__version__ import __version__
